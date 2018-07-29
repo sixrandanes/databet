41826075d9b0af3670ba492ee1b6b476dce389c3
sixrandanes <sylvain.leborgne@gmail.com>
2018-07-30 07:32:32 +1100

    feat(gateway): ajout projet

---
+++ databet-gateway/src/main/java/com/databet/gateway/DatabetGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.databet.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabetGatewayApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DatabetGatewayApplication.class, args);
+	}
+
+}
